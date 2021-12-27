7825c065211e9ca0d0403a3283205319207fc3e8
Valery <56967134+ValeriyaRom@users.noreply.github.com>
2021-12-27 20:04:13 +0300

    Update main.py

--- main.py
+++ main.py
@@ -44,6 +44,9 @@ async def root():
             <title>Рандомайзер</title>
             <link rel="stylesheet" href="static/style.css">
             <script src="static/script.js" defer></script>
+            <link rel="preconnect" href="https://fonts.googleapis.com">
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+            <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed&display=swap" rel="stylesheet">
         </head>
         <body>
             <main>
